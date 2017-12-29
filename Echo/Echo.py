c281d67022ccf7fac80ea6d0dc2127bd448928ea
maciejjg <maciej.j.grzelak@o2.pl>
2017-12-29 14:19:36 +0100

    Echo from txt file

---
+++ Echo/Echo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Write a program that will read a text file line by line.
+Each line should be brought to lowercase and the last 5 letters of the line should be repeated.
+Write the modified lines.
+"""
+
+
+def echo_text_reader(file):
+    with open(file, 'r') as text_file:
+        lines = text_file.read().split('\n')
+        lx = [[l.lower()] for l in lines]
+
+        for line in lx:
+            print(''.join(line), line[0][-5:], sep='')
+
+
+echo_text_reader("text_for_echo.txt") # at this place put file txt to repeated echo
